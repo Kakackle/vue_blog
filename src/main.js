f238b3e304e1d726a664d0cc9c38eb181b80befd
Kakackle <markowskimaciej98@gmail.com>
2023-09-29 19:41:31 +0200

    axios baseurl

--- src/main.js
+++ src/main.js
@@ -9,8 +9,7 @@ import router from './router'
 
 import axios from 'axios'
 
-// "http://127.0.0.1:8000/api/"
-axios.default.baseURL = "https://web-production-4c6b.up.railway.app/api/"
+axios.defaults.baseURL = "https://web-production-4c6b.up.railway.app/api/"
 
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css";
